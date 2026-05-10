2e41532865c48b7244e06ade7686e2bc97f17450
adijneid <adijneid@gmail.com>
2026-05-10 17:19:35 +0300

    Added comments

--- Notification.h
+++ Notification.h
@@ -4,10 +4,14 @@
 #include <string>
 using namespace std;
 
+// One alert sent to a user (e.g., new like, comment, message, friend request)
 class Notification {
 public:
+    // The user who triggered the notification
     string username;
+    // Readable description shown to the recipient
     string description;
+    // Tracked but not yet exposed in the UI; reserved for a future read-state feature
     bool isRead;
     Notification(string username, string description);
     void markAsRead();
